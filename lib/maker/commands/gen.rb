c78a35f8ad8657065903080b3453f3c887a16185
Anton <gera_box@mail.ru>
2016-11-13 22:32:36 +0300

    Full success implementation of gen command.

--- lib/maker/commands/gen.rb
+++ lib/maker/commands/gen.rb
@@ -35,33 +35,40 @@ module Maker
       if @options[:template].value != nil then
         if $global_config[:gen].keys.include?( @options[:template].value.to_sym ) then
           tmpfile = "#{$project[:systree][:config]}/#{$project[:cfgtree][:tmp]}/#{@options[:template].value}.erb"
-          # create output file.
-          f = File.new( "#{@@context[:rundir]}/#{outname}", 'w' )
-          # read template file.
-          lines = %q{}
-          File.open( tmpfile, 'r' ).each do |line|
-            lines << line
-          end
-          # output result of parsing.
-          erbout = ERB.new( lines, 0, "%<>" )
-          f.puts erbout.result
-          f.close
         else
           puts "error: unable to find template with name '#{@options[:template].value}'."
+          return
         end
       # Search template file by extension.
       else
-        #$global_config[:gen].each do |e|
-        #  e[0] == @options[:template].value
-        #  puts "key: #{e[0]}"
-        #  puts "val: #{e[1]}"
-        #end
+        ext = outname.split('.').last
+        $global_config[:gen].each do |e|
+          if e[1].include? ext then
+            tmpfile = "#{$project[:systree][:config]}/#{$project[:cfgtree][:tmp]}/#{e[0]}.erb"
+            break
+          end
+        end
+        if tmpfile == '' then
+          puts "error: unable to find template for extension '*.#{ext}'."
+          return
+        end
       end
       # Output verbose statistic.
       if global_options[:verbose].value != nil
         puts "outfile  : #{outname}"
         puts "template : #{tmpfile}"
       end
+      # create output file.
+      f = File.new( "#{@@context[:rundir]}/#{outname}", 'w' )
+      # read template file.
+      lines = %q{}
+      File.open( tmpfile, 'r' ).each do |line|
+        lines << line
+      end
+      # output result of parsing.
+      erbout = ERB.new( lines, 0, "%<>" )
+      f.puts erbout.result
+      f.close
     end
 
   end
